e464104a40c704d1179f4ea42ea1f2725feff833
Rafael Passarela <rafaelpassarela@gmail.com>
2016-08-27 11:59:33 -0300

    Adjusting compression....

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,13 +1,83 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using zlib;
 
 namespace Base64Decoder
 {
 	[Activity(Label = "Base64Decoder", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		
+
+		public static void CopyTo(Stream src, Stream dest)
+		{
+			byte[] bytes = new byte[4096];
+			int cnt;
+
+			while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+			{
+				dest.Write(bytes, 0, cnt);
+			}
+		}
+
+		private string EncriptText(string source, bool compress)
+		{
+			byte[] compressed;
+			string output;
+
+			if (compress){
+				using (var outStream = new MemoryStream())
+				{
+					using (var tinyStream = new GZipStream(outStream, CompressionMode.Compress))
+					using (var mStream = new MemoryStream(Encoding.UTF8.GetBytes(source)))
+						mStream.CopyTo(tinyStream);
+
+					compressed = outStream.ToArray();
+				}
+			}
+			else {
+				compressed = Encoding.UTF7.GetBytes(source);
+			}
+
+			var plainTextBytes = Encoding.UTF7.GetBytes(Encoding.UTF7.GetString(compressed));
+			return System.Convert.ToBase64String(plainTextBytes);
+
+			//return Encoding.UTF8.GetString(compressed);
+
+
+
+			// “compressed” now contains the compressed string.
+			// Also, all the streams are closed and the above is a self-contained operation.
+
+			using (var inStream = new MemoryStream(compressed))
+			using (var bigStream = new GZipStream(inStream, CompressionMode.Decompress))
+			using (var bigStreamOut = new MemoryStream())
+			{
+				bigStream.CopyTo(bigStreamOut);
+				output = Encoding.UTF8.GetString(bigStreamOut.ToArray());
+			}
+
+			return output;
+		}
+
+		private string DecriptText(string source)
+		{
+			byte[] bytes = Encoding.UTF8.GetBytes(source);
+			using (var msi = new MemoryStream(bytes))
+			using (var mso = new MemoryStream())
+			{
+				using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+				{
+					//gs.CopyTo(mso);
+					CopyTo(gs, mso);
+				}
+
+				return Encoding.UTF8.GetString(mso.ToArray());
+			}
+		}
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -19,10 +89,12 @@ namespace Base64Decoder
 			// and attach an event to it
 			Button btnEncript = FindViewById<Button>(Resource.Id.buttonEncript);
 			Button btnDecript = FindViewById<Button>(Resource.Id.buttonDecript);
-			TextView memoData = FindViewById<TextView>(Resource.Id.textViewData);
+			EditText memoData = FindViewById<EditText>(Resource.Id.memoValue);
+			CheckBox cbCompress = FindViewById<CheckBox>(Resource.Id.checkBox1);
 
-			btnEncript.Click += delegate {
-				memoData.Text = "Encript";
+			btnEncript.Click += delegate
+			{
+				memoData.Text = EncriptText(memoData.Text, cbCompress.Checked);
 			};
 
 			btnDecript.Click += delegate
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # base64Decoder
 for Android
 
-Just for test some things :D
+I've a Delphi app which generates some Json objects, in some cases as Base64Compressed. For do that, I'm using TCompressionStream
+of ZLib. To integrate both Android App and Delphi App I also need to use a ZLib compression on Xamarin 
+
+Input Char | Output  | Comp. Output
+A          | QQ==    | eJxzBAAAQgBC
